0b2f7afc59061708e1b654fb0fe5aa07241103f0
Megan <smith.megan.design@gmail.com>
2021-10-19 11:03:17 -0600

    cleaner code

--- server/server.py
+++ server/server.py
@@ -36,11 +36,6 @@ app=Flask(__name__, template_folder="../sleep/build")
 def members():
   return{"members":["member1","member2","member3"]}
 
-
-# @app.route("/")
-# def index():
-#   return{render_template("index.html")}
-
 if __name__=="__main__":
   app.run(debug=True)
 
